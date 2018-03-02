3df65add51c95d76bb564adb1bd107566b6dde4b
cpucko <cgpucko@gmail.com>
2018-03-02 17:03:29 -0500

    A bit more polish. And Polish.

--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -5,44 +5,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumber {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
 
         Scanner input = new Scanner(System.in);
-        String phoneNumber = input.next();
+        while (true) {
+             String phoneNumber = input.next();
+            //https://stackoverflow.com/questions/22835472/how-can-i-check-if-a-user-entered-string-matches-a-specific-format
+            Pattern format1 = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
+            Matcher matcher1 = format1.matcher(phoneNumber);
+            Pattern format2 = Pattern.compile("\\(\\d{3}\\)\\d{3}-\\d{4}");
+            Matcher matcher2 = format2.matcher(phoneNumber);
+            Pattern format3 = Pattern.compile("\\d{3}-\\d{4}");
+            Matcher matcher3 = format3.matcher(phoneNumber);
 
-        //https://stackoverflow.com/questions/22835472/how-can-i-check-if-a-user-entered-string-matches-a-specific-format
-        Pattern format1 = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
-        Matcher matcher1 = format1.matcher(phoneNumber);
-        Pattern format2 = Pattern.compile("\\(\\d{3}\\)\\d{3}-\\d{4}");
-        Matcher matcher2 = format2.matcher(phoneNumber);
-        Pattern format3 = Pattern.compile("\\d{3}-\\d{4}");
-        Matcher matcher3 = format3.matcher(phoneNumber);
-
-        if (matcher1.matches() || matcher2.matches())
-            System.out.println("Pattern Match");
-        else if (matcher3.matches())
-            System.out.println("Matches, but you will need to provide an area code for lookup.");
-        else
-            System.out.println("Nope, doofus.");
-
-
-
-
-
+            if (matcher1.matches() || matcher2.matches()) {
+                System.out.println("Pattern Match");
+                break;
+            } else if (matcher3.matches()) {
+                System.out.println("Matches, but you will need to provide an area code for lookup.");
+                break;
+            } else {
+                System.out.println("Nope, doofus.");
+            }
 
 
-
-
-
-
-
-
-
-        //Get back to this after making the string parser.
+            //Get back to this after making the string parser.
         /*Map<String, String> map = new HashMap<String, String>();
 
         map.put("dog", "is an animal");
 
         System.out.println(map.get("dog"));*/
+        }
     }
 }
